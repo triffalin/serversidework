58f41a96398d226ac9904546a289d20f29b852aa
Alin Triff <triffalin@gmail.com>
2023-09-20 18:09:08 +0300

    handled requests and created custom responses

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // 1. Execution nodejs code
 // to open this file in nodejs, typed "node app,js" in the terminal
-const userName = 'Alin';
-console.log(userName);
+// const userName = 'Alin';
+// console.log(userName);
 
 // 2. Creating a Custom nodejs server
 const http = require('http');
+// - handling request and creating custom response
+function handleRequest(request, response) {
+	response.statusCode = 200;
+	response.end('<h1>Hello World!</h1>');
+}
 
-const server = http.createServer();
+const server = http.createServer(handleRequest);
 
 server.listen(3000);
