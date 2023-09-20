3fd5da450d33b568217ae20c62d0eabf46cb2abc
Alin Triff <triffalin@gmail.com>
2023-09-20 17:55:00 +0300

    created a custom nodejs server

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const userName = 'Alin';
-
-console.log(userName);
+// 1. Execution nodejs code
 // to open this file in nodejs, typed "node app,js" in the terminal
+const userName = 'Alin';
+console.log(userName);
+
+// 2. Creating a Custom nodejs server
+const http = require('http');
+
+const server = http.createServer();
+
+server.listen(3000);
